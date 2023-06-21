879d915969da7f61da3af0c788d17b092c4b45c8
AryanPManoj <aryan.manoj2703@gmail.com>
2023-06-21 20:07:34 +0530

    change the database

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyDyrk4MGtqvNxsPMXqUys2Kzd9JtvJNB54",
+    authDomain: "voting-app-b71d6.firebaseapp.com",
+    databaseURL: "https://voting-app-b71d6-default-rtdb.firebaseio.com",
+    projectId: "voting-app-b71d6",
+    storageBucket: "voting-app-b71d6.appspot.com",
+    messagingSenderId: "251923592131",
+    appId: "1:251923592131:web:d78a8c4457f8f704454376",
+    measurementId: "G-L0RVX74CK9"
+  };
 
-// add SDK Firebase
-var firebaseConfig = {
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
